97c9652b3d7f325d86bf6ccfef119f2f402f00d6
yr.deng <yr.deng@cloudchip.com>
2017-11-22 11:28:41 +0800

    [ppcrong.20171122][cloudlib] Add okhttp/jackson dependencies and some codes

---
+++ src/main/java/com/ppcrong/cloudlib/Constant.java
@@ -0,0 +1,51 @@
+package com.ppcrong.cloudlib;
+
+public class Constant {
+
+    public static final String URL_DEMO_SERVER = "http://13.112.57.21/somewherefind/ws/";
+    public static final String URL_POSTFIX_ADD_USER = "wsAddUser.php";
+    public static final String URL_POSTFIX_ADD_USER_FB = "wsAddUserbyFB.php";
+    public static final String URL_POSTFIX_AUTH_USER = "wsAuthUser.php";
+    public static final String URL_POSTFIX_AUTH_USER_FB = "wsAuthUserbyFB.php";
+    public static final String URL_POSTFIX_EDIT_ACCOUNT = "wsEditAccount.php";
+    public static final String URL_POSTFIX_EDIT_ACCOUNT_FB = "wsEditAccountFB.php";
+    public static final String URL_POSTFIX_GET_USER_SETTING = "wsGetUserSetting.php";
+    public static final String URL_POSTFIX_EDIT_USER_SETTING = "wsEditUserSetting.php";
+    public static final String URL_POSTFIX_USER_DEVICE_ADD = "wsUserDeviceAdd.php";
+    public static final String URL_POSTFIX_USER_DEVICE_LIST = "wsUserDeviceList.php";
+    public static final String URL_POSTFIX_USER_DEVICE_DELETE = "wsUserDeviceDelete.php";
+    public static final String URL_POSTFIX_USER_GOAL_ADD = "wsUserGoalAdd.php";
+    public static final String URL_POSTFIX_USER_GOAL_LIST = "wsUserGoalList.php";
+    public static final String URL_POSTFIX_USER_GOAL_EDIT = "wsUserGoalEdit.php";
+    public static final String URL_POSTFIX_USER_GOAL_DELETE = "wsUserGoalDelete.php";
+    public static final String URL_POSTFIX_HR_ADD = "wsHeartRateAdd.php";
+    public static final String URL_POSTFIX_SLEEP_ADD = "wsSleepAdd.php";
+    public static final String URL_POSTFIX_STEP_ADD = "wsStepAdd.php";
+    public static final String URL_POSTFIX_BIKE_ADD = "wsBikeAdd.php";
+    public static final String URL_POSTFIX_SWIM_ADD = "wsSwimAdd.php";
+    public static final String URL_POSTFIX_SWIM_LAP_ADD = "wsSwimLapAdd.php";
+    public static final String URL_POSTFIX_USER_VIDEO_ADD = "wsUserVideoAdd.php";
+
+    public static final String URL_ADD_USER = URL_DEMO_SERVER + URL_POSTFIX_ADD_USER;
+    public static final String URL_ADD_USER_FB = URL_DEMO_SERVER + URL_POSTFIX_ADD_USER_FB;
+    public static final String URL_AUTH_USER = URL_DEMO_SERVER + URL_POSTFIX_AUTH_USER;
+    public static final String URL_AUTH_USER_FB = URL_DEMO_SERVER + URL_POSTFIX_AUTH_USER_FB;
+    public static final String URL_EDIT_ACCOUNT = URL_DEMO_SERVER + URL_POSTFIX_EDIT_ACCOUNT;
+    public static final String URL_EDIT_ACCOUNT_FB = URL_DEMO_SERVER + URL_POSTFIX_EDIT_ACCOUNT_FB;
+    public static final String URL_GET_USER_SETTING = URL_DEMO_SERVER + URL_POSTFIX_GET_USER_SETTING;
+    public static final String URL_EDIT_USER_SETTING = URL_DEMO_SERVER + URL_POSTFIX_EDIT_USER_SETTING;
+    public static final String URL_USER_DEVICE_ADD = URL_DEMO_SERVER + URL_POSTFIX_USER_DEVICE_ADD;
+    public static final String URL_USER_DEVICE_LIST = URL_DEMO_SERVER + URL_POSTFIX_USER_DEVICE_LIST;
+    public static final String URL_USER_DEVICE_DELETE = URL_DEMO_SERVER + URL_POSTFIX_USER_DEVICE_DELETE;
+    public static final String URL_USER_GOAL_ADD = URL_DEMO_SERVER + URL_POSTFIX_USER_GOAL_ADD;
+    public static final String URL_USER_GOAL_LIST = URL_DEMO_SERVER + URL_POSTFIX_USER_GOAL_LIST;
+    public static final String URL_USER_GOAL_EDIT = URL_DEMO_SERVER + URL_POSTFIX_USER_GOAL_EDIT;
+    public static final String URL_USER_GOAL_DELETE = URL_DEMO_SERVER + URL_POSTFIX_USER_GOAL_DELETE;
+    public static final String URL_HR_ADD = URL_DEMO_SERVER + URL_POSTFIX_HR_ADD;
+    public static final String URL_SLEEP_ADD = URL_DEMO_SERVER + URL_POSTFIX_SLEEP_ADD;
+    public static final String URL_STEP_ADD = URL_DEMO_SERVER + URL_POSTFIX_STEP_ADD;
+    public static final String URL_BIKE_ADD = URL_DEMO_SERVER + URL_POSTFIX_BIKE_ADD;
+    public static final String URL_SWIM_ADD = URL_DEMO_SERVER + URL_POSTFIX_SWIM_ADD;
+    public static final String URL_SWIM_LAP_ADD = URL_DEMO_SERVER + URL_POSTFIX_SWIM_LAP_ADD;
+    public static final String URL_USER_VIDEO_ADD = URL_DEMO_SERVER + URL_POSTFIX_USER_VIDEO_ADD;
+}
---
+++ src/main/java/com/ppcrong/cloudlib/sCloudLib.java
@@ -0,0 +1,10 @@
+package com.ppcrong.cloudlib;
+
+/**
+ * Static Cloud Library
+ */
+public class sCloudLib {
+
+    // region [Common]
+    // endregion [Common]
+}
