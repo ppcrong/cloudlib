5e623950a5fb3793f104244774dab2afa342e503
yr.deng <yr.deng@cloudchip.com>
2017-11-24 18:21:37 +0800

    [ppcrong.20171124][cloudlib] Add APIs

--- src/main/java/com/ppcrong/cloudlib/CloudLib.java
+++ src/main/java/com/ppcrong/cloudlib/CloudLib.java
@@ -3,10 +3,50 @@ package com.ppcrong.cloudlib;
 import android.util.Log;
 
 import com.ppcrong.cloudlib.callback.CloudCallback;
+import com.ppcrong.cloudlib.model.AddUserFBInput;
+import com.ppcrong.cloudlib.model.AddUserFBOutput;
 import com.ppcrong.cloudlib.model.AddUserInput;
 import com.ppcrong.cloudlib.model.AddUserOutput;
+import com.ppcrong.cloudlib.model.AuthUserFBInput;
+import com.ppcrong.cloudlib.model.AuthUserFBOutput;
 import com.ppcrong.cloudlib.model.AuthUserInput;
 import com.ppcrong.cloudlib.model.AuthUserOutput;
+import com.ppcrong.cloudlib.model.BikeAddInput;
+import com.ppcrong.cloudlib.model.BikeAddOutput;
+import com.ppcrong.cloudlib.model.EditAccountFBInput;
+import com.ppcrong.cloudlib.model.EditAccountFBOutput;
+import com.ppcrong.cloudlib.model.EditAccountInput;
+import com.ppcrong.cloudlib.model.EditAccountOutput;
+import com.ppcrong.cloudlib.model.EditUserSettingInput;
+import com.ppcrong.cloudlib.model.EditUserSettingOutput;
+import com.ppcrong.cloudlib.model.GetUserSettingInput;
+import com.ppcrong.cloudlib.model.GetUserSettingOutput;
+import com.ppcrong.cloudlib.model.HeartRateAddInput;
+import com.ppcrong.cloudlib.model.HeartRateAddOutput;
+import com.ppcrong.cloudlib.model.SleepAddInput;
+import com.ppcrong.cloudlib.model.SleepAddOutput;
+import com.ppcrong.cloudlib.model.StepAddInput;
+import com.ppcrong.cloudlib.model.StepAddOutput;
+import com.ppcrong.cloudlib.model.SwimAddInput;
+import com.ppcrong.cloudlib.model.SwimAddOutput;
+import com.ppcrong.cloudlib.model.SwimLapAddInput;
+import com.ppcrong.cloudlib.model.SwimLapAddOutput;
+import com.ppcrong.cloudlib.model.UserDeviceAddInput;
+import com.ppcrong.cloudlib.model.UserDeviceAddOutput;
+import com.ppcrong.cloudlib.model.UserDeviceDeleteInput;
+import com.ppcrong.cloudlib.model.UserDeviceDeleteOutput;
+import com.ppcrong.cloudlib.model.UserDeviceListInput;
+import com.ppcrong.cloudlib.model.UserDeviceListOutput;
+import com.ppcrong.cloudlib.model.UserGoalAddInput;
+import com.ppcrong.cloudlib.model.UserGoalAddOutput;
+import com.ppcrong.cloudlib.model.UserGoalDeleteInput;
+import com.ppcrong.cloudlib.model.UserGoalDeleteOutput;
+import com.ppcrong.cloudlib.model.UserGoalEditInput;
+import com.ppcrong.cloudlib.model.UserGoalEditOutput;
+import com.ppcrong.cloudlib.model.UserGoalListInput;
+import com.ppcrong.cloudlib.model.UserGoalListOutput;
+import com.ppcrong.cloudlib.model.UserVideoAddInput;
+import com.ppcrong.cloudlib.model.UserVideoAddOutput;
 import com.ppcrong.cloudlib.utils.Constant;
 import com.socks.library.KLog;
 
@@ -107,40 +147,171 @@ public class CloudLib {
                 });
     }
 
+    public void addUserFB(AddUserFBInput input, final CloudCallback<AddUserFBOutput> cb) {
+
+        postJson(Constant.URL_ADD_USER_FB, input, cb,
+                new TypeReference<ArrayList<AddUserFBOutput>>() {
+                });
+    }
+
     public void authUser(AuthUserInput input, final CloudCallback<AuthUserOutput> cb) {
 
         postJson(Constant.URL_AUTH_USER, input, cb,
                 new TypeReference<ArrayList<AuthUserOutput>>() {
                 });
     }
+
+    public void authUserFB(AuthUserFBInput input, final CloudCallback<AuthUserFBOutput> cb) {
+
+        postJson(Constant.URL_AUTH_USER_FB, input, cb,
+                new TypeReference<ArrayList<AuthUserFBOutput>>() {
+                });
+    }
+
+    public void editAccount(EditAccountInput input, final CloudCallback<EditAccountOutput> cb) {
+
+        postJson(Constant.URL_EDIT_ACCOUNT, input, cb,
+                new TypeReference<ArrayList<EditAccountOutput>>() {
+                });
+    }
+
+    public void editAccountFB(EditAccountFBInput input, final CloudCallback<EditAccountFBOutput> cb) {
+
+        postJson(Constant.URL_EDIT_ACCOUNT_FB, input, cb,
+                new TypeReference<ArrayList<EditAccountFBOutput>>() {
+                });
+    }
     // endregion [Account]
 
     // region [UserSetting]
+    public void getUserSetting(GetUserSettingInput input, final CloudCallback<GetUserSettingOutput> cb) {
+
+        postJson(Constant.URL_GET_USER_SETTING, input, cb,
+                new TypeReference<ArrayList<GetUserSettingOutput>>() {
+                });
+    }
+
+    public void editUserSetting(EditUserSettingInput input, final CloudCallback<EditUserSettingOutput> cb) {
+
+        postJson(Constant.URL_EDIT_USER_SETTING, input, cb,
+                new TypeReference<ArrayList<EditUserSettingOutput>>() {
+                });
+    }
     // endregion [UserSetting]
 
     // region [UserDevice]
+    public void userDeviceAdd(UserDeviceAddInput input, final CloudCallback<UserDeviceAddOutput> cb) {
+
+        postJson(Constant.URL_USER_DEVICE_ADD, input, cb,
+                new TypeReference<ArrayList<UserDeviceAddOutput>>() {
+                });
+    }
+
+    public void userDeviceList(UserDeviceListInput input, final CloudCallback<UserDeviceListOutput> cb) {
+
+        postJson(Constant.URL_USER_DEVICE_LIST, input, cb,
+                new TypeReference<ArrayList<UserDeviceListOutput>>() {
+                });
+    }
+
+    public void userDeviceDelete(UserDeviceDeleteInput input, final CloudCallback<UserDeviceDeleteOutput> cb) {
+
+        postJson(Constant.URL_USER_DEVICE_DELETE, input, cb,
+                new TypeReference<ArrayList<UserDeviceDeleteOutput>>() {
+                });
+    }
     // endregion [UserDevice]
 
     // region [UserGoal]
+    public void userGoalAdd(UserGoalAddInput input, final CloudCallback<UserGoalAddOutput> cb) {
+
+        postJson(Constant.URL_USER_GOAL_ADD, input, cb,
+                new TypeReference<ArrayList<UserGoalAddOutput>>() {
+                });
+    }
+
+    public void userGoalList(UserGoalListInput input, final CloudCallback<UserGoalListOutput> cb) {
+
+        postJson(Constant.URL_USER_GOAL_LIST, input, cb,
+                new TypeReference<ArrayList<UserGoalListOutput>>() {
+                });
+    }
+
+    public void userGoalEdit(UserGoalEditInput input, final CloudCallback<UserGoalEditOutput> cb) {
+
+        postJson(Constant.URL_USER_GOAL_EDIT, input, cb,
+                new TypeReference<ArrayList<UserGoalEditOutput>>() {
+                });
+    }
+
+    public void userGoalDelete(UserGoalDeleteInput input, final CloudCallback<UserGoalDeleteOutput> cb) {
+
+        postJson(Constant.URL_USER_GOAL_DELETE, input, cb,
+                new TypeReference<ArrayList<UserGoalDeleteOutput>>() {
+                });
+    }
     // endregion [UserGoal]
 
     // region [UploadHistory]
     // region [HR]
+    public void heartRateAdd(HeartRateAddInput input, final CloudCallback<HeartRateAddOutput> cb) {
+
+        postJson(Constant.URL_HR_ADD, input, cb,
+                new TypeReference<ArrayList<HeartRateAddOutput>>() {
+                });
+    }
     // endregion [HR]
 
     // region [Sleep]
+    public void sleepAdd(SleepAddInput input, final CloudCallback<SleepAddOutput> cb) {
+
+        postJson(Constant.URL_SLEEP_ADD, input, cb,
+                new TypeReference<ArrayList<SleepAddOutput>>() {
+                });
+    }
     // endregion [Sleep]
 
     // region [Step]
+    public void stepAdd(StepAddInput input, final CloudCallback<StepAddOutput> cb) {
+
+        postJson(Constant.URL_STEP_ADD, input, cb,
+                new TypeReference<ArrayList<StepAddOutput>>() {
+                });
+    }
     // endregion [Step]
 
     // region [Bike]
+    public void bikeAdd(BikeAddInput input, final CloudCallback<BikeAddOutput> cb) {
+
+        postJson(Constant.URL_BIKE_ADD, input, cb,
+                new TypeReference<ArrayList<BikeAddOutput>>() {
+                });
+    }
     // endregion [Bike]
 
     // region [Swim]
+    public void swimAdd(SwimAddInput input, final CloudCallback<SwimAddOutput> cb) {
+
+        postJson(Constant.URL_SWIM_ADD, input, cb,
+                new TypeReference<ArrayList<SwimAddOutput>>() {
+                });
+    }
+
+    public void swimLapAdd(SwimLapAddInput input, final CloudCallback<SwimLapAddOutput> cb) {
+
+        postJson(Constant.URL_SWIM_LAP_ADD, input, cb,
+                new TypeReference<ArrayList<SwimLapAddOutput>>() {
+                });
+    }
     // endregion [Swim]
 
     // region [Video]
+    public void userVideoAdd(UserVideoAddInput input, final CloudCallback<UserVideoAddOutput> cb) {
+
+        postJson(Constant.URL_USER_VIDEO_ADD, input, cb,
+                new TypeReference<ArrayList<UserVideoAddOutput>>() {
+                });
+    }
     // endregion [Video]
     // endregion [UploadHistory]
 }
